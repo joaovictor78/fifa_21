3e897fbafd83dcb61c9d5ec0ec42d0ae5e5752d3
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-11-20 19:11:45 -0400

    feat: add start page

---
+++ lib/modules/start/start_page.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../design_system/styles/color_palettes.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  ValueNotifier<int> selectedPageValueNotifier = ValueNotifier<int>(0);
+
+  @override
+  void initState() {
+    super.initState();
+    Modular.to.navigate('/start/players');
+    Modular.to.addListener(onChangeRoute);
+  }
+
+  void onChangeRoute() {
+    if (Modular.to.path.contains('players')) {
+      selectedPageValueNotifier.value = 0;
+    }
+
+    if (Modular.to.path.contains('best_players')) {
+      selectedPageValueNotifier.value = 1;
+    }
+
+    if (Modular.to.path.contains('teams')) {
+      selectedPageValueNotifier.value = 2;
+    }
+  }
+
+  @override
+  void dispose() {
+    Modular.to.removeListener(onChangeRoute);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: const RouterOutlet(),
+          bottomNavigationBar: Theme(
+              data: Theme.of(context).copyWith(
+                canvasColor: Theme.of(context).primaryColor,
+                primaryColor: Theme.of(context).colorScheme.secondary,
+                textTheme: Theme.of(context).textTheme.copyWith(
+                    caption: TextStyle(color: ColorPalettes.accentPrimary)),
+              ),
+              child: StatefulBuilder(
+                  builder: (context, setState) => SizedBox(
+                        height: 80,
+                        child: BottomAppBar(
+                          child: ValueListenableBuilder(
+                            valueListenable: selectedPageValueNotifier,
+                            builder: (context, selectedPage, child) => Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Modular.to.navigate("/start/players");
+                                    },
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Icon(
+                                          Icons.home_rounded,
+                                          size: 35,
+                                          color: selectedPage == 0
+                                              ? ColorPalettes.accentPrimary
+                                              : ColorPalettes.black12,
+                                        ),
+                                        Text(
+                                          "Players",
+                                          style: GoogleFonts.rubik(
+                                              fontWeight: selectedPage == 0
+                                                  ? FontWeight.w800
+                                                  : FontWeight.w400,
+                                              color: Colors.black),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Modular.to
+                                          .navigate("/start/best_players");
+                                    },
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Icon(
+                                          Icons.settings,
+                                          size: 35,
+                                          color: selectedPage == 1
+                                              ? ColorPalettes.accentPrimary
+                                              : ColorPalettes.black12,
+                                        ),
+                                        Text(
+                                          "Best Players",
+                                          style: GoogleFonts.rubik(
+                                              fontWeight: selectedPage == 1
+                                                  ? FontWeight.w800
+                                                  : FontWeight.w400,
+                                              color: Colors.black),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Modular.to.navigate("/start/teams");
+                                    },
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Icon(
+                                          Icons.settings,
+                                          size: 35,
+                                          color: selectedPage == 1
+                                              ? ColorPalettes.accentPrimary
+                                              : ColorPalettes.black12,
+                                        ),
+                                        Text(
+                                          "Teams",
+                                          style: GoogleFonts.rubik(
+                                              fontWeight: selectedPage == 1
+                                                  ? FontWeight.w800
+                                                  : FontWeight.w400,
+                                              color: Colors.black),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )))),
+    );
+  }
+}
