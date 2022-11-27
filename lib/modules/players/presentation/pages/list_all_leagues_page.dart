41b691d9f25a4812fea3837e28bf05260b6eda8d
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-11-27 15:50:18 -0400

    feat: add search for leagues name in page

--- lib/modules/players/presentation/pages/list_all_leagues_page.dart
+++ lib/modules/players/presentation/pages/list_all_leagues_page.dart
@@ -24,7 +24,7 @@ class _ListAllLeaguesPageState extends State<ListAllLeaguesPage> {
   @override
   void initState() {
     bloc = Modular.get<LeaguesBloc>();
-    bloc.add(FetchFilterTypesItems());
+    bloc.add(FetchFilterTypesItems(page: _page));
     FocusManager.instance.primaryFocus?.unfocus();
     _scrollController.addListener(_onScroll);
     super.initState();
@@ -34,99 +34,117 @@ class _ListAllLeaguesPageState extends State<ListAllLeaguesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorPalettes.lightPrimary,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20, bottom: 20),
-              child: Row(
-                children: [
-                  InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Icon(
-                        Icons.arrow_back_ios_outlined,
-                        color: Colors.white,
-                      )),
-                  const SizedBox(width: 10),
-                  Text(
-                    'Leagues',
-                    style: GoogleFonts.poppins(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400),
-                  )
-                ],
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                child: Row(
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.arrow_back_ios_outlined,
+                          color: Colors.white,
+                        )),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Leagues',
+                      style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Filter By Leagues',
-                style: GoogleFonts.poppins(fontSize: 14, color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Filter By Leagues',
+                  style: GoogleFonts.poppins(fontSize: 14, color: Colors.white),
+                ),
               ),
-            ),
-            SearchBarWidget(
-                hintText: "Search for a team...", onChanged: (value) {}),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Center(
-                child: BlocBuilder(
-                    bloc: bloc,
-                    builder: (context, ResultFilterTypeItemsByLeague state) {
-                      if (state.status == FilterTypeItemsStatus.initial) {
-                        return Center(
-                          child: CircularProgressIndicator(
-                            color: ColorPalettes.accentPrimary,
-                          ),
-                        );
-                      }
-                      if (bloc.state.leagues?.isEmpty == true) {
-                        return Center(
-                            child: Text(
-                          "No leagues available",
-                          style: GoogleFonts.poppins(color: Colors.white),
-                        ));
-                      }
-                      return ListView.builder(
-                          controller: _scrollController,
-                          itemCount: bloc.state.leagues?.length == null
-                              ? 0
-                              : bloc.state.leagues!.length - 1,
-                          itemBuilder: ((context, index) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: ListTile(
-                                onTap: () {
-                                  Modular.to.pushNamed(
-                                      '/players/list_players/leagues',
-                                      arguments: {
-                                        'id': state.leagues?[index].id ?? 0
-                                      });
-                                },
-                                title: Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: Text(
-                                    state.leagues?[index].name ?? '',
-                                    style: GoogleFonts.poppins(
-                                        color: Colors.white),
+              SearchBarWidget(
+                  hintText: "Search for a league...",
+                  onChanged: (value) {
+                    _page = 1;
+                    if (value.length > 1) {
+                      bloc.add(FetchFilterTypesItems(
+                          page: _page, name: value, isSearch: true));
+                    } else {
+                      bloc.add(FetchFilterTypesItems(
+                        page: _page,
+                        name: '',
+                        isSearch: true,
+                      ));
+                    }
+                  }),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Center(
+                  child: BlocBuilder(
+                      bloc: bloc,
+                      builder: (context, ResultFilterTypeItemsByLeague state) {
+                        if (state.status == FilterTypeItemsStatus.initial) {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: ColorPalettes.accentPrimary,
+                            ),
+                          );
+                        }
+                        if (bloc.state.leagues?.isEmpty == true) {
+                          return Center(
+                              child: Text(
+                            "No leagues available",
+                            style: GoogleFonts.poppins(color: Colors.white),
+                          ));
+                        }
+                        return ListView.builder(
+                            controller: _scrollController,
+                            itemCount: bloc.state.leagues?.length == null
+                                ? 0
+                                : bloc.state.leagues!.length - 1,
+                            itemBuilder: ((context, index) {
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: ListTile(
+                                  onTap: () {
+                                    Modular.to.pushNamed(
+                                        '/players/list_players/leagues',
+                                        arguments: {
+                                          'id': state.leagues?[index].id ?? 0
+                                        });
+                                  },
+                                  title: Padding(
+                                    padding: const EdgeInsets.only(left: 8.0),
+                                    child: Text(
+                                      state.leagues?[index].name ?? '',
+                                      style: GoogleFonts.poppins(
+                                          color: Colors.white),
+                                    ),
                                   ),
+                                  tileColor: ColorPalettes.accentPrimary,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
                                 ),
-                                tileColor: ColorPalettes.accentPrimary,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                              ),
-                            );
-                          }));
-                    }),
-              ),
-            ))
-          ],
+                              );
+                            }));
+                      }),
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
