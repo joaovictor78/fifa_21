853b867b0700893935e2cdf9804e74c6fd0d81c7
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-11-27 15:39:52 -0400

    feat: add search by nationality in bloc

--- lib/modules/players/presentation/bloc/nationalities_bloc.dart
+++ lib/modules/players/presentation/bloc/nationalities_bloc.dart
@@ -18,25 +18,35 @@ class NationalitiesBloc
     if (state.hasReachedMax) return;
     if (state.status == FilterTypeItemsStatus.initial) {
       final nationalitiesOrFailure =
-          await nationalitiesRepository.findAllNationalities(page: event.page);
+          await (event.isSearch && event.name.isNotEmpty
+              ? nationalitiesRepository.findAllNationalitiesByName(
+                  nationalityName: event.name, page: 1)
+              : nationalitiesRepository.findAllNationalities(page: event.page));
       nationalitiesOrFailure.fold(
           (l) => emit(state.copyWith(status: FilterTypeItemsStatus.failure)),
           (r) => emit(ResultFilterItemsTypeByNationality(
               status: FilterTypeItemsStatus.success,
               hasReachedMax: false,
               nationalities: List.of(state.nationalities ?? [])..addAll(r))));
+      return;
     }
     final nationalitiesOrFailure =
-        await nationalitiesRepository.findAllNationalities(page: event.page);
+        await (event.isSearch && event.name.isNotEmpty
+            ? nationalitiesRepository.findAllNationalitiesByName(
+                nationalityName: event.name, page: 1)
+            : nationalitiesRepository.findAllNationalities(page: event.page));
 
     nationalitiesOrFailure.fold(
         (l) => emit(ResultFilterItemsTypeByNationality(
             status: FilterTypeItemsStatus.failure)),
-        (r) => r.isEmpty
+        (r) => !event.isSearch && r.isEmpty
             ? emit(state.copyWith(hasReachedMax: true))
             : emit(state.copyWith(
                 status: FilterTypeItemsStatus.success,
-                nationalities: List.of(state.nationalities ?? [])..addAll(r),
+                nationalities: event.isSearch
+                    ? (List.of([])..addAll(r))
+                    : List.of(state.nationalities ?? [])
+                  ..addAll(r),
                 hasReachedMax: false,
               )));
   }
