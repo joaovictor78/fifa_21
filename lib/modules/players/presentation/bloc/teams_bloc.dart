9d8090d6fd9869a1e9ff230a7f07083118e49f58
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-11-27 15:43:28 -0400

    fix: change  to search for teams  only if name is different from empty

--- lib/modules/players/presentation/bloc/teams_bloc.dart
+++ lib/modules/players/presentation/bloc/teams_bloc.dart
@@ -15,25 +15,32 @@ class TeamsBloc extends Bloc<ListFilterTypeEvent, ResultFilterTypeItemsByTeam> {
   Future<void> _fetchAllTeams(FetchFilterTypesItems event, Emitter emit) async {
     if (state.hasReachedMax) return;
     if (state.status == FilterTypeItemsStatus.initial) {
-      final teamsOrFailure =
-          await teamsRepository.findAllTeams(page: event.page);
+      final teamsOrFailure = await (event.isSearch && event.name.isNotEmpty
+          ? teamsRepository.findAllTeamsByName(teamName: event.name, page: 1)
+          : teamsRepository.findAllTeams(page: event.page));
       teamsOrFailure.fold(
           (l) => emit(state.copyWith(status: FilterTypeItemsStatus.failure)),
           (r) => emit(ResultFilterTypeItemsByTeam(
               status: FilterTypeItemsStatus.success,
               hasReachedMax: false,
               teams: List.of(state.teams ?? [])..addAll(r))));
+      return;
     }
-    final teamsOrFailure = await teamsRepository.findAllTeams(page: event.page);
+    final teamsOrFailure = await (event.isSearch && event.name.isNotEmpty
+        ? teamsRepository.findAllTeamsByName(teamName: event.name, page: 1)
+        : teamsRepository.findAllTeams(page: event.page));
 
     teamsOrFailure.fold(
         (l) => emit(ResultFilterTypeItemsByLeague(
             status: FilterTypeItemsStatus.failure)),
-        (r) => r.isEmpty
+        (r) => !event.isSearch && r.isEmpty
             ? emit(state.copyWith(hasReachedMax: true))
             : emit(state.copyWith(
                 status: FilterTypeItemsStatus.success,
-                teams: List.of(state.teams ?? [])..addAll(r),
+                teams: event.isSearch
+                    ? (List.of([])..addAll(r))
+                    : List.of(state.teams ?? [])
+                  ..addAll(r),
                 hasReachedMax: false,
               )));
   }
